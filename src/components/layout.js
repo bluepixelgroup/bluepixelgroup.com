8a1c88f931a0fe49c90fe84a564faf1e31f79286
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-13 15:30:47 +0200

    Added support for SASS styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react"
 
+import "./layout.scss"
+
 export default ({ children }) => {
   return (
     <React.Fragment>
-      <div>This is from layout component</div>
+      <div className="test">This is from layout component</div>
       <div>{children}</div>
     </React.Fragment>
   )
