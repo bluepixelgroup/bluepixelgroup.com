3facd43dc8ddd2082d0203c08c225c874a687548
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-13 15:00:09 +0200

    Added layout component

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default ({ children }) => {
+    return (
+        <React.Fragment>
+            <div>This is from layout component</div>
+            <div>{ children }</div>
+        </React.Fragment>
+    );
+}
