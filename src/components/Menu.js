02b7a0f0095aedf37bcff102437949c5f280f59c
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-13 17:34:47 +0200

    Added skeleton for the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Button } from "@material-ui/core"
+import { Grid, Button } from "@material-ui/core"
 
 import BluePixelLogo from '../assets/images/logo.svg'
 
 export default () => {
   return (
     <header>
-      <BluePixelLogo/>
-      <Link to="/">SERVICES</Link>
-      <Link to="/">CLIENTS</Link>
-      <Link to="/">TEAM</Link>
-      <Link to="/">CONTACT US</Link>
-      <Button variant="outlined" color="primary">START A PROJECT</Button>
+      <Grid container direction="row" alignItems="center" justify="space-between">
+        <Grid item>
+          <Link to="/">
+            <BluePixelLogo/>
+          </Link>
+        </Grid>
+        <Grid item>
+          <Grid container direction="row" alignItems="center" spacing={3}>
+            <Grid item>
+              <Link to="/">SERVICES</Link>
+            </Grid>
+            <Grid item>
+              <Link to="/">CLIENTS</Link>
+            </Grid>
+            <Grid item>
+              <Link to="/">TEAM</Link>
+            </Grid>
+            <Grid item>
+              <Link to="/">CONTACT US</Link>
+            </Grid>
+            <Grid item>
+              <Button variant="outlined" color="primary">START A PROJECT</Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </header>
   )
 }
