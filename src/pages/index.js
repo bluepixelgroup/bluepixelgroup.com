b1e887338074d6a831c995de20cc000fddfbe356
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-13 15:03:58 +0200

    Added editorconfig file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 
-
 export default () => {
-    return (
-        <Layout>
-            <h1>Hello world!</h1>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <h1>Hello world!</h1>
+    </Layout>
+  )
 }
