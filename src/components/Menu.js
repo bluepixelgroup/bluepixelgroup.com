3e35a62013273aba005efe98b974980ee10a3d97
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-14 17:05:58 +0200

    Added mobile version of the menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,40 +1,161 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Grid, Button } from "@material-ui/core"
+import {
+  Grid,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  IconButton,
+  Hidden,
+} from "@material-ui/core"
 
 import BluePixelLogo from "../assets/images/logo.svg"
+import HamburgerIcon from "../assets/images/hamburger.svg"
+import CloseHamburgerIcon from "../assets/images/close-hamburger.svg"
 
 import "./Menu.scss"
 
-export default () => {
-  return (
-    <header className="menu">
-      <Grid container direction="row" alignItems="center" justify="space-between">
-        <Grid item>
-          <Link to="/">
-            <BluePixelLogo/>
-          </Link>
+export default class Menu extends React.Component {
+  state = {
+    drawerOpened: false,
+  }
+
+  toggleDrawer = opened => {
+    this.setState({ drawerOpened: opened })
+  }
+
+  renderMobileMenu = () => {
+    let { drawerOpened } = this.state
+
+    return (
+      <header className="menu">
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          justify="space-between"
+        >
+          <Grid item>
+            <Link to="/">
+              <BluePixelLogo />
+            </Link>
+          </Grid>
+          <Grid item>
+            <IconButton
+              color="primary"
+              aria-label="open drawer"
+              edge="start"
+              onClick={() => this.toggleDrawer(true)}
+            >
+              <HamburgerIcon />
+            </IconButton>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Grid container direction="row" alignItems="center" spacing={2}>
-            <Grid item className="menu-item">
-              <Link to="/">SERVICES</Link>
-            </Grid>
-            <Grid item className="menu-item">
-              <Link to="/">CLIENTS</Link>
-            </Grid>
-            <Grid item className="menu-item">
-              <Link to="/">TEAM</Link>
-            </Grid>
-            <Grid item className="menu-item">
-              <Link to="/">CONTACT US</Link>
+        <Drawer
+          anchor="top"
+          open={drawerOpened}
+          onClick={() => this.toggleDrawer(false)}
+        >
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            className="mobile-menu"
+          >
+            <Grid item>
+              <Grid container justify="space-between" alignItems="center">
+                <Grid item>
+                  <Link to="/">
+                    <BluePixelLogo />
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <CloseHamburgerIcon
+                    onClick={() => {
+                      this.toggleDrawer(false)
+                    }}
+                  />
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item>
-              <Button variant="outlined" color="primary">START A PROJECT</Button>
+              <List>
+                <ListItem className="mobile-menu-item">
+                  <Link to="/">SERVICES</Link>
+                </ListItem>
+                <ListItem className="mobile-menu-item">
+                  <Link to="/">CLIENTS</Link>
+                </ListItem>
+                <ListItem className="mobile-menu-item">
+                  <Link to="/">TEAM</Link>
+                </ListItem>
+                <ListItem className="mobile-menu-item">
+                  <Link to="/">CONTACT US</Link>
+                </ListItem>
+                <ListItem className="mobile-menu-item">
+                  <Button variant="outlined" color="primary">
+                    START A PROJECT
+                  </Button>
+                </ListItem>
+              </List>
+            </Grid>
+          </Grid>
+        </Drawer>
+      </header>
+    )
+  }
+
+  renderDesktopMenu = () => {
+    return (
+      <header className="menu">
+        <Grid
+          container
+          direction="row"
+          alignItems="center"
+          justify="space-between"
+        >
+          <Grid item>
+            <Link to="/">
+              <BluePixelLogo />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Grid container direction="row" alignItems="center" spacing={2}>
+              <Grid item className="menu-item">
+                <Link to="/">SERVICES</Link>
+              </Grid>
+              <Grid item className="menu-item">
+                <Link to="/">CLIENTS</Link>
+              </Grid>
+              <Grid item className="menu-item">
+                <Link to="/">TEAM</Link>
+              </Grid>
+              <Grid item className="menu-item">
+                <Link to="/">CONTACT US</Link>
+              </Grid>
+              <Grid item>
+                <Button variant="outlined" color="primary">
+                  START A PROJECT
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </header>
-  )
+      </header>
+    )
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Hidden only={['sx', 'sm']}>
+          { this.renderDesktopMenu() }
+        </Hidden>
+        <Hidden only={['md', 'lg', 'xl']}>
+          { this.renderMobileMenu() }
+        </Hidden>
+      </React.Fragment>
+    )
+  }
 }
--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -17,9 +17,13 @@ const theme = createMuiTheme({
     },
   },
   overrides: {
+    MuiPaper: {
+      root: {
+        backgroundColor: `#FAFBFC`,
+      }
+    },
     MuiButton: {
       outlinedPrimary: {
-        height: '40px',
         border: '2px solid #2B72F5',
         fontWeight: 'bold',
         '&:hover': {
