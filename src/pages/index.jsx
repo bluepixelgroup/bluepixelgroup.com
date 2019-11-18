fbe6ad5ade24718c1b10b7ab790ad45ae7772ed3
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-18 12:24:12 +0200

    Use .jsx instead of .js for react components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Hero from "../components/Hero";
 import Technology from "../components/Technology";
 import OneStop from "../components/OneStop";
