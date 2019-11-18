082abc505287407d3f2105f7fbeceb6ebcd0c73f
alexd <daicu.alexandru@gmail.com>
2019-11-18 12:19:26 +0200

    Add cloud providers.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 import Typewriter from "typewriter-effect";
 import Container from "@material-ui/core/Container";
-
 import {Button} from "@material-ui/core"
+import Grid from "@material-ui/core/Grid";
+
+import GoogleSVG from "../assets/images/landing-hero/google.inline.svg";
+import DigitalOceanSVG
+  from "../assets/images/landing-hero/digital-ocean.inline.svg";
+import AmazonSVG from "../assets/images/landing-hero/aws.inline.svg";
+import AzureSVG from "../assets/images/landing-hero/azure.inline.svg";
 import "./Hero.scss";
 
 export default () => {
@@ -36,6 +42,28 @@ export default () => {
             start a project
           </Button>
           <div className="btn-info">* Free quote for your project</div>
+        </div>
+        <div className="building-on">
+          <h6>Building on:</h6>
+          <Grid container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+                className="cloud-providers"
+          >
+            <Grid container item xs={6} sm={3} spacing={3}>
+              <AmazonSVG className="cloud-icon"/>
+            </Grid>
+            <Grid container item xs={6} sm={3} spacing={3}>
+              <DigitalOceanSVG className="cloud-icon"/>
+            </Grid>
+            <Grid container item xs={6} sm={3} spacing={3}>
+              <GoogleSVG className="cloud-icon"/>
+            </Grid>
+            <Grid container item xs={6} sm={3} spacing={3}>
+              <AzureSVG className="cloud-icon"/>
+            </Grid>
+          </Grid>
         </div>
       </Container>
     </div>
