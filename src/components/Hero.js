ba0877c273dce86580d7fda3bb86abb26e61ebac
alexd <daicu.alexandru@gmail.com>
2019-11-18 11:34:04 +0200

    Add loop to hero typing animation.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Typing from "react-typing-animation";
+import Typewriter from "typewriter-effect";
 import Container from "@material-ui/core/Container";
-import {Button} from "@material-ui/core"
 
+import {Button} from "@material-ui/core"
 import "./Hero.scss";
 
 export default () => {
@@ -13,13 +13,17 @@ export default () => {
         <div className="content-wrapper">
           <h1>
             The fastest and most convenient way to&nbsp;
-            <Typing className="typings">
-              <span>deliver your software.</span>
-              <Typing.Backspace count={22} delay={400}/>
-              <span>build great products.</span>
-              <Typing.Backspace count={21} delay={400}/>
-              <span>something else here.</span>
-            </Typing>
+            <span className="typings">
+              <Typewriter
+                options={{
+                  strings: ['deliver your software.',
+                    'build great products.',
+                    'something else here.'],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
           </h1>
           <div>
             Many desktop publishing packages and web page editors now use Lorem
