a2e51359de6af5902fdf4186b98b7df2c6ffd600
Vitalie Maldur <maldur.vitalie@gmail.com>
2019-11-18 10:57:39 +0200

    Removed hardcoded height on body

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,18 +2,17 @@ import React from "react"
 import Menu from "./Menu"
 import Footer from "./Footer"
 
-import {Container} from "@material-ui/core";
-import "./layout.scss"
+import { Container } from "@material-ui/core"
 
 export default ({ children }) => {
   return (
     <React.Fragment>
       <Container>
-        <Menu/>
+        <Menu />
       </Container>
-      { children }
+      {children}
       <Container>
-        <Footer/>
+        <Footer />
       </Container>
     </React.Fragment>
   )
